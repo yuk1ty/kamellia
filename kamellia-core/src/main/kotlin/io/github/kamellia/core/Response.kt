17a80924e92ffd08e48dce0ce7aede09d19e874f
yuk1ty <yuk1ty@users.noreply.github.com>
2026-06-18 22:43:34 +0900

    Add documentation to Response class

--- kamellia-core/src/main/kotlin/io/github/kamellia/core/Response.kt
+++ kamellia-core/src/main/kotlin/io/github/kamellia/core/Response.kt
@@ -1,5 +1,24 @@
 package io.github.kamellia.core
 
+/**
+ * An HTTP response.
+ *
+ * This is the concrete representation of a response that is sent back to the client.
+ * It can be created directly or produced by calling [IntoResponse.intoResponse].
+ *
+ * Example:
+ * ```kotlin
+ * val response = Response(
+ *     status = HttpStatus.OK,
+ *     headers = mapOf("Content-Type" to "text/plain"),
+ *     body = Body.Strict("Hello, World!".toByteArray())
+ * )
+ * ```
+ *
+ * @property status The HTTP status code of the response.
+ * @property headers The HTTP headers to include in the response.
+ * @property body The body of the response.
+ */
 data class Response(val status: HttpStatus, val headers: Headers = emptyMap(), val body: Body = Body.Empty) :
     IntoResponse {
     override fun intoResponse(): Response = this
